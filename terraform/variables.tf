7f8adb149f1c1b223d82f5a61338f5ce810f03e1
ZsoltTirkala <tirkala.zsolt@gmail.com>
2021-04-20 19:07:14 +0200

    Adding variables for EC2 and ECR

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,33 @@ variable "s3_bucket_acl" {
   type        = string
   default     = "public-read"
 }
+
+variable "image_id" {
+  description = "EC2 ami's id"
+  type        = string
+  default     = "ami-096cb92bb3580c759" //  Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 free tier instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ec2_key_pair" {
+  description = "Key pair to the EC2 instance"
+  type        = string
+  default     = "il-homework-zs"
+}
+
+variable "ec2_instance_name" {
+  description = "The Name tag for the EC2 instance"
+  type        = string
+  default     = "il-ec2-host-zs"
+}
+
+variable "ecr_repository_name" {
+  description = "ECR repository's name"
+  type        = string
+  default     = "il-homework-zs"
+}
